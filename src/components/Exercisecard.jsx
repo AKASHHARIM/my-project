e97aa5a9b732b4e9e8b15a510b83c0c229f6f9a2
AKASHHARIM <167068172+AKASHHARIM@users.noreply.github.com>
2025-01-30 16:22:09 +0530

    2nd commit

--- src/components/Exercisecard.jsx
+++ src/components/Exercisecard.jsx
@@ -48,7 +48,7 @@ function Exercisecard(props) {
                         </div>
                     )
                 })}
-                <button onClick={handleSetIncrement} className='flex flex-col p-2 rounded border-[1.5px] duration-200 border-solid border-blue-900 hover:border-blue-600 w-full duration-200'>
+                <button onClick={handleSetIncrement} className='flex flex-col p-2 rounded border-[1.5px] duration-200 border-solid border-blue-900 hover:border-blue-600 w-full'>
                     <h3 className='text-slate-400 text-sm capitalize'>Sets completed</h3>
                     <p className='font-medium'>{setsCompleted} / 5</p>
                 </button>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Button from './Button'
-import '../hero.css'
+import '../Hero.css'
 
 function Hero() {
   return (
